93dceffa968d530a3bfe78d11a72be3f9844e506
Níck <nicolasiesus10@gmail.com>
2025-05-19 02:13:46 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,28 @@
-document.addEventListener('DOMContentLoaded', function(){
-  
-})
+AOS.init();
+
+const dataDoEvento = new Date('May 23, 2025 18:00:00');
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const time = setInterval(function() {
+  const agora = new Date();
+  const timeStampAtual = agora.getTime();
+
+  const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+  const diasEmMs = 1000 * 60 * 60 * 24;
+  const horasEmMs = 1000 * 60 * 60;
+  const minutosEmMs = 1000 * 60;
+
+  const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+  const horasAteOEvento = Math.floor((distanciaAteOEvento % diasEmMs) / horasEmMs);
+  const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMs) / minutosEmMs);
+  const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / 1000);
+
+  document.getElementById('count').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+  if(distanciaAteOEvento < 0) {
+    clearInterval(time);
+    document.getElementById('count').innerHTML = 'Evento expirado'
+  }
+
+}, 1000)
